8a92fc626d51a506be93b74dab09a995739bfae7
Veera Mikola <vmikola@ppelogin2.ppe.gla.ac.uk>
2025-10-09 15:00:49 +0100

    More cleaning

--- NOvAT2K_OscProbComparisons.py
+++ NOvAT2K_OscProbComparisons.py
@@ -5,11 +5,10 @@ import uproot
 
 custom_labels = {"inputs/T2K_inputs/oscillation_weights_allzero_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "PMNS Model", "inputs/T2K_inputs/oscillation_weights_epsemuonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_emu = 0.25 delta_emu=0", "inputs/T2K_inputs/oscillation_weights_epsemudeltaemupi2_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_emu = 0.25 delta_emu=pi/2", "inputs/T2K_inputs/oscillation_weights_epsemudeltaemupi_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_emu = 0.25 delta_emu=pi", "inputs/T2K_inputs/oscillation_weights_epsemudeltaemu3pi2_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_emu = 0.25 delta_emu=3pi/2"}
 
-#custom_labels = {"New_inputs/oscillation_weights_allzero_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "PMNS Model", "New_inputs/oscillation_weights_epseeonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_ee = 0.25", "New_inputs/oscillation_weights_epsemuonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_emu = 0.25", "New_inputs/oscillation_weights_epsetauonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_etau = 0.25", "New_inputs/oscillation_weights_epsmumuonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_mumu = 0.25", "New_inputs/oscillation_weights_epsmutauonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_mutau = 0.25", "New_inputs/oscillation_weights_epstautauonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_tautau = 0.25"}
+#custom_labels = {"inputs/T2K_inputs/oscillation_weights_allzero_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "PMNS Model", "inputs/T2K_inputs/oscillation_weights_epseeonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_ee = 0.25", "inputs/T2K_inputs/oscillation_weights_epsemuonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_emu = 0.25", "inputs/T2K_inputs/oscillation_weights_epsetauonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_etau = 0.25", "inputs/T2K_inputs/oscillation_weights_epsmumuonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_mumu = 0.25", "inputs/T2K_inputs/oscillation_weights_epsmutauonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_mutau = 0.25", "inputs/T2K_inputs/oscillation_weights_epstautauonly_onlyecouplings_NOvAbaseline_deltaCPfix.txt" : "eps_tautau = 0.25"}
 
 
 text_files = list(custom_labels.keys())
-
 
 # Function to find closest key within tolerance
 def get_closest_value(dictionary, key, tol=1e-3):
@@ -106,61 +105,6 @@ for idx, (file_name, label) in enumerate(custom_labels.items()):
     output_path = f"Diffplots_NOvAbaseline_nue_{label_clean}.jpg"
     plt.savefig(output_path, format="jpg", dpi=300)
     plt.close()
-        
-# linestyles = ["-", "--"]
-
-# for file_name, (energy, probability) in data.items():
- #    if file_name == "New_inputs/oscillation_weights_epsemudeltaemupi_onlyecouplings_NOvAbaseline_deltaCPfix.txt":
-   # style = linestyles[0] if file_name == "New_inputs/oscillation_weights_allzero_onlyecouplings.txt" else linestyles[1]
- #        label = custom_labels.get(file_name, file_name)
- #        ax1.plot(energy, probability, linestyle = "-", markersize=1.5, label="T2K (OscProb)") #'o'  linemarker = style
-  #   else:
-   #      continue
-
-# for name, xy_dict in graphs.items():
-  #   if name == "prob_3;1":
-    #     x_vals = list(xy_dict.keys())
-      #   y_vals = list(xy_dict.values())
-  #       ax1.plot(x_vals, y_vals, linestyle='-', label="NOvA (OscLib)")
- #    else:
- #        continue
-
-# ax1.set_xlabel("Energy (GeV)")
-# ax1.set_ylabel("Probability")
-# ax1.legend()
-# ax1.set_ylim(0, 0.1)
-#ax1.set_ylim(0, 1.2)
-# ax1.set_xlim(0.25, 4)
-# ax1.grid(True)
-
-# energy, probability_data = data["New_inputs/oscillation_weights_epsemudeltaemupi_onlyecouplings_NOvAbaseline_deltaCPfix.txt"]
-# xy_dict = graphs["prob_3;1"]
-
-# Match energy values and compute ratio
-# ratio = []
-# for e, p_data in zip(energy, probability_data):
- #    p_graph = get_closest_value(xy_dict, e)
-  #   if p_graph is not None and p_graph != 0:
-        #(OscLib-OscProb)/Osc_Prob
-  #       ratio.append((p_data-p_graph)/p_graph)
-        #print(p_data, p_graph)
- #    else:
- #        ratio.append(float('nan'))  # Avoid division by zero 
-        
-# Plot the ratio
-# ax2.plot(energy, ratio, marker = 'o', linestyle='None', color='purple', label='Data / Graph Ratio')
-# ax2.set_title('Ratio')
-# ax2.set_xlabel('Energy (GeV)')
-# ax2.set_ylabel('Difference')
-#ax2.set_ylim(-0.2, 0.2)
-#ax2.set_xlim(0, 4)
-# ax2.grid(True)
-#ax2.legend()
-
-# Save and show the combined plot
-# plt.tight_layout()
-# plt.savefig("Comparison_plots/Diffplots_NOvAbaseline_nue_epsemudeltaemupi_025limit_CPfix.jpg", format="jpg", dpi=300)
-# plt.show()
 
 
 
